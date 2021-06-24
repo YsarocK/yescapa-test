0b76699c5d410aa10b4d9988be66ed4b463019c3
Etienne <emoureton@gmail.com>
2021-06-24 14:14:22 +0200

    firstcommit

---
+++ components/VehiculeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vehicule-card-container">
+    <p>{{ this.vehicule.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'vehicule-card',
+   props: ['vehicule'],
+}
+</script>
+
+<style>
+.vehicule-card-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+     <div v-for="vehicule of vehicules.results" :key="vehicule.title">
+       <vehicule-card :vehicule="vehicule"></vehicule-card>
+     </div>
+  </div>
+</template>
+
+<script>
+import VehiculeCard from '../components/VehiculeCard.vue';
+export default {
+  components: { VehiculeCard },
+    async asyncData({ $http }) {
+      let vehicules = await $http.$get(`https://gitlab.com/api/v4/snippets/2095016/raw`);
+      return { vehicules }
+    }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
